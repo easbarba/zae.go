c757fd59dfb014d32865901c68cb7a3be7576e6b
easbarba <easbarba@outlook.com>
2022-12-11 11:54:35 -0300

    cmd: implement cli parsing

--- cmd/zae/main.go
+++ cmd/zae/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/alecthomas/kong"
 )
 
@@ -14,8 +12,116 @@ func main() {
 	ctx.FatalIfErrorf(err)
 }
 
+var cli struct {
+	Debug bool `help:"Enable debug mode."`
+
+	Clean     CleanCmd     `cmd help:"clean system residual packages dependencies"`
+	Deps      DepsCmd      `cmd help:"install dependencies packages of package"`
+	Depends   DependsCmd   `cmd help:"list all dependencies of package"`
+	Download  DownloadCmd  `cmd help:"download package binary"`
+	Fix       FixCmd       `cmd help:"fix system package manager issues"`
+	Help      HelpCmd      `cmd help:"shows a list of commands or help for one command"`
+	Info      InfoCmd      `cmd help:"view info about a specific package"`
+	Install   InstallCmd   `cmd help:"install package(s) from repositories"`
+	Installed InstalledCmd `cmd help:"list all installed packages on system"`
+	Remove    RemoveCmd    `cmd help:"remove one or more installed packages"`
+	Search    SearchCmd    `cmd help:"search for matching packages of term."`
+	Update    UpdateCmd    `cmd help:"update packages database"`
+	UpGrade   UpgradeCmd   `cmd help:"upgrade installed packages"`
+}
+
 type Context struct {
 	Debug bool
+}
+
+type CleanCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *CleanCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type DepsCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *DepsCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type DependsCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *DependsCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type DownloadCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *DownloadCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type FixCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *FixCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type HelpCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *HelpCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type InfoCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *InfoCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type InstallCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *InstallCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type InstalledCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *InstalledCmd) Run(ctx *Context) error {
+
+	return nil
+}
+
+type RemoveCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
+
+func (r *RemoveCmd) Run(ctx *Context) error {
+
+	return nil
 }
 
 type SearchCmd struct {
@@ -26,7 +132,7 @@ type SearchCmd struct {
 }
 
 func (r *SearchCmd) Run(ctx *Context) error {
-	fmt.Println("searching:", r.Program)
+
 	return nil
 }
 
@@ -35,13 +141,14 @@ type UpdateCmd struct {
 }
 
 func (r *UpdateCmd) Run(ctx *Context) error {
-	fmt.Println("updating:", r.Program)
+
 	return nil
 }
 
-var cli struct {
-	Debug bool `help:"Enable debug mode."`
+type UpgradeCmd struct {
+	Program string `arg name:"program" optional:"" help:"Program to remove." type:"string"`
+}
 
-	Search SearchCmd `cmd help:"search for matching packages of term."`
-	Update UpdateCmd `cmd help:"update single package, or all system packages."`
+func (r *UpgradeCmd) Run(ctx *Context) error {
+	return nil
 }
